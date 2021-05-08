0b2e916cba2a37a347cbf07af37b483a2ec751fb
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-05-08 20:45:17 +0100

    Update

--- apirest/src/main/java/com/tqs/apirest/model/Cities.java
+++ apirest/src/main/java/com/tqs/apirest/model/Cities.java
@@ -5,60 +5,61 @@ import javax.persistence.*;
 @Entity(name="cities")
 @Table(name="city")
 public class Cities {
+
     @Id
     @GeneratedValue
     private Long idgerated;
 
     // Cidade
-    private Long idx; //Unique ID for the city monitoring station.
+    private Long idx;
 
-    @Column(nullable=false, unique=false)
-    private String name; //Name of the monitoring station.
+    @Column(nullable=false)
+    private String name;
 
-    @Column(nullable=false, unique=false)
-    private String timestamp; //Tempo da leitura
+    @Column(nullable=false)
+    private String timestamp;
 
     // Valores
-    @Column(nullable=false, unique=false)
-    private Double aqi; //Real-time air quality information.
+    @Column(nullable=false)
+    private Double airQuality;
 
 
     // Valores gases
-    // Valores podem ser null ou optional ou wtv pq ha cidades q n tem
-    @Column(nullable=true, unique=false)
-    private Double pm25;  //PM 2.5
 
-    @Column(nullable=true, unique=false)
-    private Double pm10;  //PM 10
+    @Column(nullable=true)
+    private Double pm25;                    //PM 2.5
 
-    @Column(nullable=true, unique=false)
-    private Double o3;    //Ozono
+    @Column(nullable=true)
+    private Double pm10;                    //PM 10
 
-    @Column(nullable=true, unique=false)
-    private Double no2; //Dióxido de nitrogénio
+    @Column(nullable=true)
+    private Double o3;                      //Ozono
 
-    @Column(nullable=true, unique=false)
-    private Double so2; //Dióxido de enxofre
+    @Column(nullable=true)
+    private Double no2;                     //Dióxido de nitrogénio
+
+    @Column(nullable=true)
+    private Double so2;                     //Dióxido de enxofre
 
 
     // Valores temperatura
-    @Column(nullable=true, unique=false)
-    private Double temperature;     //Temperature
+    @Column(nullable=true)
+    private Double temperature;             //Temperature
 
-    @Column(nullable=true, unique=false)
-    private Double pressure;     //Pressure
+    @Column(nullable=true)
+    private Double pressure;                //Pressure
 
-    @Column(nullable=true, unique=false)
-    private Double h;     //Humidity
+    @Column(nullable=true)
+    private Double h;                       //Humidity
 
-    @Column(nullable=true, unique=false)
-    private Double w;     //Wind
+    @Column(nullable=true)
+    private Double w;                       //Wind
 
-    public Cities(Long idx, String name, String timestamp, Double aqi, Double pm25, Double pm10, Double o3, Double no2, Double so2, Double temperature, Double pressure, Double h, Double w) {
+    public Cities(Long idx, String name, String timestamp, Double airQuality, Double pm25, Double pm10, Double o3, Double no2, Double so2, Double temperature, Double pressure, Double h, Double w) {
         this.idx = idx;
         this.name = name;
         this.timestamp = timestamp;
-        this.aqi = aqi;
+        this.airQuality = airQuality;
         this.pm25 = pm25;
         this.pm10 = pm10;
         this.o3 = o3;
@@ -88,8 +89,8 @@ public class Cities {
         return timestamp;
     }
 
-    public Double getAqi() {
-        return aqi;
+    public Double getAirQuality() {
+        return airQuality;
     }
 
     public Double getPm25() {
@@ -135,7 +136,7 @@ public class Cities {
                 ", idx=" + idx +
                 ", name='" + name + '\'' +
                 ", timestamp='" + timestamp + '\'' +
-                ", aqi=" + aqi +
+                ", airQuality=" + airQuality +
                 ", pm25=" + pm25 +
                 ", pm10=" + pm10 +
                 ", o3=" + o3 +
